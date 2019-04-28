1b7a0e3dd2ace2ce1fa10b4bf82f9d16851c624b
agretta <grexxaa@gmail.com>
2019-04-28 18:22:43 -0400

    Fleshed out code for server and ReconSphere to hopefully workable parts

--- ReconSphere/ReconSphere.ino
+++ ReconSphere/ReconSphere.ino
@@ -8,12 +8,13 @@
 // Accelerometer globals
 BMA250 accel_sensor;
 int x, y, z;
-int prev_x, prev_y, prev_z;
+int init_x, init_y, init_z;
 double x_rot, y_rot;
-//256 is roughly gravity
+// Note: 256 is roughly gravity
 double gravity_mag = 0.0;
 
-// IR reader globals
+
+// IR Reader globals
 const byte nbSensors = 4;
 const byte medianFilterWindowSize = 3;
 SharpDistSensor sensorArray[] = {
@@ -22,24 +23,26 @@ SharpDistSensor sensorArray[] = {
   SharpDistSensor(A4, medianFilterWindowSize),
   SharpDistSensor(A5, medianFilterWindowSize),
 };
-
 uint16_t distArray[nbSensors];
+int directionArray[nbSensors][3] = {{0,0,1},
+                                    {0,0,-1},
+                                    {0,1,0},
+                                    {0,-1,0}};
+                                     
 
 //WiFi globals
-char ssid[] = "TinyZeroTest";  //  your network SSID (name)
-char pass[] = "gt123456";      // your network password
-int status = WL_IDLE_STATUS;   // the WiFi radio's status
-
+char ssid[] = "TinyZeroTest";  // network SSID
+char pass[] = "gt123456";      // network password
+int status = WL_IDLE_STATUS;   // WiFi radio status
 IPAddress server(192,168,43,233);
 int server_port = 8347;
-
-IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
 WiFiUDP Udp;
-//char packetBuffer[255]; //buffer to hold incoming packet
 unsigned int localPort = 2390;
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[255];
+const char* delim = "|";
+
+
 
 void setup() {
   SerialUSB.begin(9600);
@@ -49,21 +52,115 @@ void setup() {
   Wire.begin();
   accel_sensor.begin(BMA250_range_2g, BMA250_update_time_64ms);   
  
-  gravity_mag = callibrateToGravity();  
-
-  //initIRSensors();
+  initIRSensors();
 
   connectToWiFi();
   Udp.begin(localPort);
- }
+  
+  delay(500);  
+}
 
 
 void loop() {
-  // listNetworks();
+  bool run_scan = false;
+
+  // Wait for a start packet to begin scanning
+  run_scan = checkContinueScan();
+
+  // Send the initial gravity reading to the server
+  if (run_scan) {
+    char c[11];
+    itoa(init_x,c,10);
+    itoa(init_y,&(c[4]),10);
+    itoa(init_z,&(c[8]),10);
+    c[3] = *delim;
+    c[7] = *delim;
+    SerialUSB.println(c);
+    sendPacket(c, 11);
+  }
   
-  char c[] = "Hello World";
-  sendPacket(c);
-  delay(3000);
+  while(run_scan) {
+    
+    //temp delay   
+    delay(10000);
+    
+    // Read from the accelorometer to determine current posistion 
+    bool accel_error = false;
+    accel_sensor.read();
+    x = accel_sensor.X;
+    y = accel_sensor.Y;
+    z = accel_sensor.Z;
+
+    // Error checking
+    if (x == -1 && y == -1 && z == -1) {
+      SerialUSB.print("ERROR! NO BMA250 DETECTED!");
+      accel_error = true;
+    } else {
+      double x_Buff = float(x);
+      double y_Buff = float(y);
+      double z_Buff = float(z);
+      y_rot = atan2(y_Buff , z_Buff) * 57.3;
+      x_rot = atan2((- x_Buff) , sqrt(y_Buff * y_Buff + z_Buff * z_Buff)) * 57.3;
+      SerialUSB.print("Roll:");
+      SerialUSB.println(y_rot);
+      SerialUSB.print("Pitch:");
+      SerialUSB.println(x_rot);
+      showSerial();
+    }
+
+    // scan the world with the IR sensors
+    if (!accel_error) {
+      SerialUSB.println("SENSOR READING:");
+      for (byte i = 0; i < nbSensors; i++) {
+        unsigned int distance = sensorArray[i].getDist();
+        SerialUSB.println(distance);
+        SerialUSB.println(analogRead(A1));
+        distArray[i] = distance;
+      }
+    }
+
+    // Construct and send the scanned data packet to the server
+    if (!accel_error) {
+      char c[31];
+      // gravity reading
+      itoa(x,c,10);
+      itoa(y,&(c[4]),10);
+      itoa(z,&(c[7]),10);
+      
+      // sensor readings
+      itoa(distArray[0],&(c[12]),10);
+      itoa(distArray[1],&(c[17]),10);
+      itoa(distArray[2],&(c[22]),10);
+      itoa(distArray[3],&(c[27]),10);
+      
+      c[3] = *delim;
+      c[7] = *delim;
+      c[11] = *delim;
+      c[16] = *delim;
+      c[21] = *delim;
+      c[26] = *delim;
+      SerialUSB.println(c);
+      sendPacket(c, 31);
+    }
+    
+    // The BMA250 can only poll new sensor values every 64ms
+    delay(70);
+    
+    // if the sphere recived a end packet, stop scanning and reset
+    run_scan = checkContinueScan();    
+  }
+
+}
+
+/*
+ * Checks for an incoming packet indicating whether or not the ReconSphere
+ * should begin or end the scanning process, also inits/resets the gravity measurement
+ * Returns a boolean indicating whether or not the ReconSphere should be scanning
+ */
+ 
+bool checkContinueScan() {
+  bool continue_scan = false;
+
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
@@ -75,82 +172,48 @@ void loop() {
     SerialUSB.print(", port ");
     SerialUSB.println(Udp.remotePort());
 
-    // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 14);
+    int len = Udp.read(packetBuffer, packetSize);
     if (len > 0) packetBuffer[len] = 0;
     SerialUSB.println("Contents:");
-//    SerialUSB.println(packetBuffer);
+    SerialUSB.println((char *)packetBuffer);
+    if ((char *)packetBuffer == "START") {
+      continue_scan = true;
+      gravity_mag = callibrateToGravity();  
+    }
+    if ((char *)packetBuffer == "END") {
+      continue_scan = false;
+      gravity_mag = 0;
+      init_x, init_y, init_z = 0;
+    }
   }
-  delay(10000); // Wait a bit before going back through main loop
-
-  /*accel_sensor.read();
-  x = accel_sensor.X;
-  y = accel_sensor.Y;
-  z = accel_sensor.Z;
-
-  // error checking
-  if (x == -1 && y == -1 && z == -1) {
-    SerialUSB.print("ERROR! NO BMA250 DETECTED!");
-  } else {
-    double x_Buff = float(x);
-    double y_Buff = float(y);
-    double z_Buff = float(z);
-    y_rot = atan2(y_Buff , z_Buff) * 57.3;
-    x_rot = atan2((- x_Buff) , sqrt(y_Buff * y_Buff + z_Buff * z_Buff)) * 57.3;
-    /*SerialUSB.print("Roll:");
-    SerialUSB.println(y_rot);
-    SerialUSB.print("Pitch:");
-    SerialUSB.println(x_rot);
-    //SerialUSB.print("Grav Mag:");
-    //SerialUSB.println(gravity_mag);
-    showSerial();
-  }
-  SerialUSB.println("SENSOR READING:");
-  for (byte i = 0; i < nbSensors; i++) {
-    unsigned int distance = sensorArray[i].getDist();
-    //xSerialUSB.println(distance);
-    //SerialUSB.println(analogRead(pin1));
-    distArray[i] = distance;
-  }
-
-  // The BMA250 can only poll new sensor values every 64ms
-  delay(250);*/
-}
-
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Udp.beginPacket(server, server_port);
-  Udp.write("Hello World", 12);
-  Udp.endPacket();
+  return continue_scan;
 }
 
 /*
  * Sends a UDP packet to the server specified in the globals
  */
-void sendPacket(char packetData[]) {
+void sendPacket(char packetData[], int packet_size) {
   Udp.beginPacket(server, server_port);
-  Udp.write(packetData, 12);
+  Udp.write(packetData, packet_size);
   Udp.endPacket();
   SerialUSB.println("Sending packet"); 
   SerialUSB.println(packetData);
 }
 
 /*
- * Sets up the custom function for decoding voltage to a distance
+ * Sets up the custom 5 variable polynomial function for decoding analog voltage to a distance in mm
  * also initilzes posistions of sensors with relation to sphere
  */
 void initIRSensors() {
   SerialUSB.print("Initializing IR Sensors...");
   // Custom callibration of 3.3V ir sensor based on calibration data
   const float polyCoefficients[] = {1015.787378, -56.37879315, 1.475534708, -1.85676847E-2,  1.103053715E-4, -2.479657003E-7};
-  const byte nbCoefficients = 6;  // Number of coefficients
-  const unsigned int minVal = 80; // ~800 mm
-  const unsigned int maxVal = 875; // ~50mm
+  const byte nbCoefficients = 6;
+  const unsigned int minVal = 80; // ~1500 mm
+  const unsigned int maxVal = 875; // ~10mm
 
   for (byte i = 0; i < nbSensors; i++) {
     sensorArray[i].setPolyFitCoeffs(nbCoefficients, polyCoefficients, minVal, maxVal);
-    // Set other parameters as required
   }
 }
 
@@ -170,7 +233,10 @@ double callibrateToGravity() {
     SerialUSB.print("ERROR! NO BMA250 DETECTED!");
     while(true);
   } else {
-    gravity_mag = sqrt(x_t*x_t + y_t*y_t + z_t*z_t);  
+    gravity_mag = sqrt(x_t*x_t + y_t*y_t + z_t*z_t); 
+    init_x = x_t;
+    init_y = y_t;
+    init_z = z_t;
   }   
   delay(250);
   
@@ -212,18 +278,15 @@ void printWiFiData() {
   SerialUSB.print("IP Address: ");
   SerialUSB.println(ip);
   SerialUSB.println(ip);
-
   // print your MAC address:
   byte mac[6];
   WiFi.macAddress(mac);
   SerialUSB.print("MAC address: ");
   printMacAddress(mac);
-
   // print your subnet mask:
   IPAddress subnet = WiFi.subnetMask();
   SerialUSB.print("NetMask: ");
   SerialUSB.println(subnet);
-
   // print your gateway address:
   IPAddress gateway = WiFi.gatewayIP();
   SerialUSB.print("Gateway: ");
@@ -234,18 +297,15 @@ void printCurrentNet() {
   // print the SSID of the network you're attached to:
   SerialUSB.print("SSID: ");
   SerialUSB.println(WiFi.SSID());
-
   // print the MAC address of the router you're attached to:
   byte bssid[6];
   WiFi.BSSID(bssid);
   SerialUSB.print("BSSID: ");
   printMacAddress(bssid);
-
   // print the received signal strength:
   long rssi = WiFi.RSSI();
   SerialUSB.print("signal strength (RSSI):");
   SerialUSB.println(rssi);
-
   // print the encryption type:
   byte encryption = WiFi.encryptionType();
   SerialUSB.print("Encryption Type:");
--- server.py
+++ server.py
@@ -1,20 +1,75 @@
 #!/usr/bin/python3
+import time 
 import socket
+import threading
 
+print("Initializing ReconSphere Data Server")
 # Create a UDP socket for this server
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = 8347
 serversocket.bind(('', port))
 
-# Run the Server
-print("Running Server")
+arduino_ip = "143.215.115.187"
+arduino_port = 2390
+
+data = []
+init_gravity = []
+
+user_input = [None]
+
+# spawn a new thread to wait for input 
+def get_user_input(user_input_ref):
+    while user_input_ref[0] != 'end':
+        user_input_ref[0] = input("Type 'end' to stop scan: ")
+
+
 while True:
 
-    s = "Hello Arduino"
-    # serversocket.sendto(s.encode("utf8"), ("143.215.115.187" , 7));
-    msg, addr = serversocket.recvfrom(1024)
-    # decoded_msg = msg.decode("utf8");
-    # decoded_msg = decoded_msg.split('|')
-    print(msg);
-    if msg == 'end':
-        close = True
+    start = ''
+    start = input("Enter start to begin scan: ")
+    if start == 'start':
+
+        # start the thread looking for the end input
+        mythread = threading.Thread(target=get_user_input, args=(user_input,))
+        mythread.daemon = True
+        mythread.start()
+
+        run = True
+        s = 'start';
+        # TODO: check encoding on both sides
+        serversocket.sendto(s.encode("utf8"), (arduino_ip , arduino_port));
+
+        # get the init gravity reading
+        # msg, addr = serversocket.recvfrom(1024)
+        msg = "111|222|333"
+        decoded_msg = msg.split('|')
+        init_gravity = decoded_msg
+        print(init_gravity)
+
+        # recieve distance data from the ReconSphere, and plot it
+        while run:
+            # msg, addr = serversocket.recvfrom(1024)
+            # decoded_msg = msg.decode("utf8");
+            msg = "111|222|333|1234|2345|3456|4567"
+            decoded_msg = msg.split('|')
+            print(decoded_msg);
+            print(msg);
+            data.append([ decoded_msg[0:3] , decoded_msg[3] , decoded_msg[4] , decoded_msg[5] , decoded_msg[6] ]);
+
+            #########################################
+            # TODO: Do the graphing thing here, and fancy math
+            #########################################
+
+            # maybe if the ball detects that its done, or erred, have it end here
+            if msg == 'end':
+                run = False;
+                print("AN ERROR OCCURED")
+            # user typed end, so stop the server 
+            if user_input[0] == 'end':
+                user_input[0] = None
+                run = False
+                s = "end"
+                serversocket.sendto(s.encode("utf8"), (arduino_ip , arduino_port));
+                print("Halting ReconSphere...")
+                time.sleep(2)
+
