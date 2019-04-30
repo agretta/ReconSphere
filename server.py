5303e941c9b23e9e46b315fb2184ad63a192d783
agretta <grexxaa@gmail.com>
2019-04-30 14:17:28 -0400

    hopefully plotting works

--- server.py
+++ server.py
@@ -2,28 +2,18 @@
 import time
 import socket
 import threading
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# ax.
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
 
-
-print("Initializing ReconSphere Data Server")
-# Create a UDP socket for this server
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-port = 8347
-serversocket.bind(('', port))
-
-arduino_ip = "192.168.43.251"
-arduino_port = 2390
-
-data = []
-init_gravity = []
-
-user_input = [None]
+def angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 # spawn a new thread to wait for input 
 def get_user_input(user_input_ref):
@@ -31,8 +21,27 @@ def get_user_input(user_input_ref):
         user_input_ref[0] = input("Type 'end' to stop scan: ")
 
 
-while True:
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
+# ax.scatter(x,y,z)
+
+print("Initializing ReconSphere Data Server")
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = 8347
+serversocket.bind(('', port))
+
+arduino_ip = "192.168.43.251"
+arduino_port = 2390
+
+raw_data = []
+init_gravity = []
+
+data_points = []
+sensor_pos = [[0,0,1],[0,0,-1],[1,0,0],[-1,0,0]]
+
+user_input = [None]
+while True:
     start = ''
     start = input("Enter start to begin scan: ")
     if start == 'start':
@@ -65,13 +74,26 @@ while True:
             decoded_msg = msg.split('|')
             print(decoded_msg);
             print(msg);
-            data.append([ decoded_msg[0:3] , decoded_msg[3] , decoded_msg[4] , decoded_msg[5] , decoded_msg[6] ]);
+            raw_data.append([ decoded_msg[0:3] , decoded_msg[3] , decoded_msg[4] , decoded_msg[5] , decoded_msg[6] ]);
 
             #########################################
             # TODO: Do the graphing thing here, and fancy math
             #########################################
 
+            raw_data[0]
+            angle = angle_between(init_gravity, raw_data[-1][0])
+            rot = [[1,0,0],[0,np.cos(angle),-np.sin(angle)],[0,np.sin(angle),np.cos(angle)]]
 
+            for i in range(0,4):
+                sensor = sensor_pos[i]
+                sen_dist = sensor * raw_data[-1][i+1]
+                print(sen_dist)
+                new_sensor_pos = np.multiply(sen_dist, rot)
+                print(new_sensor_pos)
+                new_sensor_pos[1] += 10
+                data_points.append(new_sensor_pos)
+
+            ax.scatter(data_points[:,0],y[:,1],z[:,2])
 
             # maybe if the ball detects that its done, or erred, have it end here
             if msg == 'end':
